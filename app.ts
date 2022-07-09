0c3688ca8ad85880b3324203ab1b5e6011c77e00
Steve <steve@posmarket.com.au>
2022-07-10 09:45:51 +1000

    Add api server for movies and the Cinema class

--- app.ts
+++ app.ts
@@ -1,3 +1,6 @@
+import { response } from "express"
+import { IncomingMessage } from "http"
+
 console.log("Hello, AWP")
 
 enum STATUS {
@@ -34,12 +37,13 @@ class Seat {
     }    
 }
 
+
 class Row {
     id: number
     seats: Seat[]
     element: HTMLDivElement
 
-    constructor(id: number, seatNumber: number) {
+    constructor(id: number, seatNumber: number, occupiedSeats: number[] = []) {
         this.id = id
         this.seats = Array.from({ length: seatNumber }).map((_, index) => {
             const seatId = seatNumber * id + index
@@ -60,9 +64,9 @@ class SeatMap {
     selectedSeats: number[] = []
     element: HTMLElement
 
-    constructor(rowNumber: number, seatNumberPerRow: number) {
+    constructor(rowNumber: number, seatNumberPerRow: number, occupiedSeats: number[] = []) {
         this.rows = Array.from({ length: rowNumber}).map((_, index) => {
-            return new Row(index, seatNumberPerRow)
+            return new Row(index, seatNumberPerRow, occupiedSeats)
         })
         this.element = document.createElement('div')
         this.element.classList.add('seat-map')
@@ -81,6 +85,111 @@ class SeatMap {
     }    
 }
 
+class Cinema
+{
+    movies: Movie[] = []
+    seatMap: SeatMap | null = null
+    theaterContainer: HTMLDivElement
+    moviesContainer: HTMLDivElement
+    movieElements: HTMLDivElement[] = []
+
+    constructor() {
+        this.moviesContainer = document.createElement('div')
+        this.moviesContainer.id = 'movies'
+        this.moviesContainer.classList.add('movies')
+
+        this.theaterContainer = document.createElement('div')
+        this.theaterContainer.id = 'theater'
+        this.theaterContainer.classList.add('theater')
+
+        const screenElement = document.createElement('div')
+        screenElement.classList.add('screen')
+
+        this.theaterContainer.appendChild(screenElement)
+        document.getElementById('cinema')?.append(this.moviesContainer, this.theaterContainer)
+    }
+
+    async fetchMovies() {
+        this.movies = await http<Movie[]>('http://localhost:8000/movies')
+        if (this.movies.length) {
+            this.renderMovies()
+            this.selectMovie(0)
+        }
+    }    
+
+    renderMovies() {
+        this.movieElements = []
+        this.moviesContainer.innerHTML = ''
+        this.movies.forEach((movie, index) => {
+            const movieElement = document.createElement('div')
+            movieElement.id = movie.id.toString()
+            movieElement.classList.add('movie')
+            movieElement.addEventListener('click', () => {
+                if (!movieElement.classList.contains('selected')) {
+                    this.selectMovie(index)
+                }
+            })
+            const posterElement = document.createElement('img')
+            posterElement.classList.add('poster')
+            posterElement.src = movie.poster
+
+            const titleElement = document.createElement('div')
+            titleElement.classList.add('title')
+            titleElement.innerText = movie.title
+
+            movieElement.append(posterElement, titleElement)
+            this.movieElements.push(movieElement)
+            this.moviesContainer.appendChild(movieElement)
+        })
+    }
+    
+    selectMovie(selectedIndex: number) {
+        this.movieElements.forEach((element, index) =>
+            selectedIndex === index
+                ?element.classList.add('selected')
+                : element.classList.remove('selected')
+        )
+        const {
+            theater: { rowNumber, seatNumberPerRow, occupiedSeats },
+        } = this.movies[selectedIndex]
+        if (this.seatMap) {
+            this.theaterContainer.lastChild?.remove()
+        }
+        this.seatMap = new SeatMap(rowNumber, seatNumberPerRow, occupiedSeats)
+        this.theaterContainer.append(this.seatMap.element)        
+    }    
+}
+
 const seatMap = new SeatMap(7, 9)
 
 document.getElementById('theater')?.appendChild(seatMap.element)
+
+type Theater = {
+    rowNumber: number
+    seatNumberPerRow: number
+    occupiedSeats?: number[]
+}
+
+type Movie = {
+    id: number
+    title: string
+    poster: string
+    theater: Theater
+}
+
+const cinema = new Cinema()
+cinema.fetchMovies()
+
+// async function http<T>(request: RequestInfo): Promise<T> {
+//     const response = await fetch(request)
+//     if (!response.ok) {
+//         throw new Error(response.statusText)
+//     }
+//     const headers = response.headers
+//     const data = headers.get('content-type')?.includes('json')
+//         ? await response.json
+//         : {}
+//     return data
+// }
+
+// fetchMovies()
